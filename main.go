84062d5899e329b04be3af8882c439b9f849a3b3
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2023-05-24 01:28:11 -0700

    Upgrade go program

--- main.go
+++ main.go
@@ -1,128 +1,242 @@
 package main
 
-/*
-#include <stdlib.h>
-
-extern int32_t GetMeasurementInt(void *context, int name);
-extern int32_t GetMeasurementBytes(void *context, int name, int buf, int buflen);
-extern void SetMeasurementInt(void *context, int name, int value);
-extern void AddToMeasurementInt(void *context, int name, int value);
-extern void SetMeasurementBytes(void *context, int name, int value, int size);
-extern void ForwardInterest(void *context, int faceID);
-*/
-import "C"
 import (
 	"fmt"
-	"reflect"
-	"unsafe"
+	"os"
 
 	"github.com/wasmerio/wasmer-go/wasmer"
 )
 
-var measure map[string]interface{}
-var instance wasmer.Instance
+const WASM_PROG = "./csrc/sample.wasm"
+
+type WasmEnv struct {
+	inst    *wasmer.Instance
+	measure map[string]interface{}
+	memory  *wasmer.Memory
+
+	allocate             wasmer.NativeFunction
+	deallocate           wasmer.NativeFunction
+	afterReceiveInterest wasmer.NativeFunction
+}
 
 // Given we have multiple instances in YaNFD, we need to figure out which one
 // A global variable is not a good idea
 
-//export ForwardInterest
-func ForwardInterest(context unsafe.Pointer, faceID int32) {
-	// I don't quite understand what is "context". It only occurs in some examples.
+func ForwardInterest(env interface{}, args []wasmer.Value) ([]wasmer.Value, error) {
+	faceID := args[0].I32()
 	fmt.Printf("Forwarded to: %d\n", faceID)
+	return []wasmer.Value{}, nil
 }
 
-func getWasmStr(ptr int32) string {
-	return C.GoString((*C.char)(unsafe.Pointer(&instance.Memory.Data()[int(ptr)])))
+func getWasmStr(memData []byte, ptr int32) string {
+	ed := int(ptr)
+	for i := int(ptr); i < len(memData); i++ {
+		if memData[i] == 0 {
+			ed = i
+			break
+		}
+	}
+	return string(memData[ptr:ed])
 }
 
-//export GetMeasurementInt
-func GetMeasurementInt(context unsafe.Pointer, name int32) int32 {
-	nameStr := getWasmStr(name)
+func GetMeasurementInt(env interface{}, args []wasmer.Value) ([]wasmer.Value, error) {
+	name := args[0].I32()
+	measure := env.(*WasmEnv).measure
+	memory := env.(*WasmEnv).memory
+	nameStr := getWasmStr(memory.Data(), name)
+
 	// Will panic if it is not an int
-	return measure[nameStr].(int32)
+	result := measure[nameStr].(int32)
+
+	return []wasmer.Value{wasmer.NewI32(result)}, nil
 }
 
-//export SetMeasurementInt
-func SetMeasurementInt(context unsafe.Pointer, name int32, val int32) {
-	nameStr := getWasmStr(name)
+func SetMeasurementInt(env interface{}, args []wasmer.Value) ([]wasmer.Value, error) {
+	name := args[0].I32()
+	val := args[1].I32()
+	measure := env.(*WasmEnv).measure
+	memory := env.(*WasmEnv).memory
+	nameStr := getWasmStr(memory.Data(), name)
+
 	measure[nameStr] = val
+
+	return []wasmer.Value{}, nil
 }
 
-//export AddToMeasurementInt
-func AddToMeasurementInt(context unsafe.Pointer, name int32, val int32) {
-	nameStr := getWasmStr(name)
+func AddToMeasurementInt(env interface{}, args []wasmer.Value) ([]wasmer.Value, error) {
+	name := args[0].I32()
+	val := args[1].I32()
+	measure := env.(*WasmEnv).measure
+	memory := env.(*WasmEnv).memory
+	nameStr := getWasmStr(memory.Data(), name)
+
 	cur := measure[nameStr].(int)
 	measure[nameStr] = cur + int(val)
+
+	return []wasmer.Value{}, nil
 }
 
-func pointerToSlice(ptr unsafe.Pointer, length int) (ret []byte) {
-	header := (*reflect.SliceHeader)(unsafe.Pointer(&ret))
-	header.Data = uintptr(ptr)
-	header.Cap = length
-	header.Len = length
-	return
-}
+func GetMeasurementBytes(env interface{}, args []wasmer.Value) ([]wasmer.Value, error) {
+	name := args[0].I32()
+	buf := args[1].I32()
+	buflen := args[2].I32()
+	measure := env.(*WasmEnv).measure
+	memory := env.(*WasmEnv).memory
+	nameStr := getWasmStr(memory.Data(), name)
 
-//export GetMeasurementBytes
-func GetMeasurementBytes(context unsafe.Pointer, name int32, buf int32, buflen int32) int32 {
-	nameStr := getWasmStr(name)
 	val := measure[nameStr].([]byte)
-	bufPtr := unsafe.Pointer(&instance.Memory.Data()[int(buf)])
-	return int32(copy(pointerToSlice(bufPtr, int(buflen)), val))
+	ret := int32(copy(memory.Data()[buf:buf+buflen], val))
+
+	return []wasmer.Value{wasmer.NewI32(ret)}, nil
 }
 
-//export SetMeasurementBytes
-func SetMeasurementBytes(context unsafe.Pointer, name int32, value int32, size int32) {
-	nameStr := getWasmStr(name)
-	bufPtr := unsafe.Pointer(&instance.Memory.Data()[int(value)])
-	// C.GoBytes copy the content of bufPtr
-	data := C.GoBytes(bufPtr, C.int(size))
+func SetMeasurementBytes(env interface{}, args []wasmer.Value) ([]wasmer.Value, error) {
+	name := args[0].I32()
+	value := args[1].I32()
+	size := args[2].I32()
+	measure := env.(*WasmEnv).measure
+	memory := env.(*WasmEnv).memory
+	nameStr := getWasmStr(memory.Data(), name)
+
+	data := make([]byte, size)
+	copy(data, memory.Data()[value:value+size])
 	measure[nameStr] = data
+
+	return []wasmer.Value{}, nil
 }
 
-func callAfterReceiveInterest(name string) {
+func callAfterReceiveInterest(env *WasmEnv, name string) {
 	// Allocate the string in WASM memory
 	// len(name) is the length in bytes, not in characters.
-	allocateResult, _ := instance.Exports["Allocate"](len(name))
-	inputPointer := allocateResult.ToI32()
-	defer instance.Exports["Deallocate"](inputPointer)
+	allocateResult, _ := env.allocate(int32(len(name)))
+	inputPointer := allocateResult.(int32)
+	defer env.deallocate(inputPointer)
 
-	strPtr := unsafe.Pointer(&instance.Memory.Data()[int(inputPointer)])
-	copy(pointerToSlice(strPtr, len(name)), name)
+	ptr := int(inputPointer)
+	copy(env.memory.Data()[ptr:ptr+len(name)], name)
+	env.memory.Data()[ptr+len(name)] = 0
 
-	_, err := instance.Exports["AfterReceiveInterest"](inputPointer, len(name))
+	_, err := env.afterReceiveInterest(inputPointer, int32(len(name)))
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func main() {
-	importObj := wasmer.NewDefaultWasiImportObject()
-	imports, err := importObj.Imports()
-	imports, _ = imports.AppendFunction("GetMeasurementInt", GetMeasurementInt, C.GetMeasurementInt)
-	imports, _ = imports.AppendFunction("GetMeasurementBytes", GetMeasurementBytes, C.GetMeasurementBytes)
-	imports, _ = imports.AppendFunction("ForwardInterest", ForwardInterest, C.ForwardInterest)
-	imports, _ = imports.AppendFunction("SetMeasurementInt", SetMeasurementInt, C.SetMeasurementInt)
-	imports, _ = imports.AppendFunction("AddToMeasurementInt", AddToMeasurementInt, C.AddToMeasurementInt)
-	imports, _ = imports.AppendFunction("SetMeasurementBytes", SetMeasurementBytes, C.SetMeasurementBytes)
+	// Create an Engine
+	engine := wasmer.NewEngine()
 
-	// Reads the WebAssembly module as bytes.
-	bytes, _ := wasmer.ReadBytes("./csrc/sample.wasm")
+	// Create a Store
+	store := wasmer.NewStore(engine)
+
+	fmt.Println("Compiling module...")
+	wasmBytes, _ := os.ReadFile(WASM_PROG)
+	module, err := wasmer.NewModule(store, wasmBytes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	measure := make(map[string]interface{})
+	measure["counter"] = 0
+	measure["hash"] = []byte{0, 0, 0, 0}
+	env := &WasmEnv{
+		measure: measure,
+	}
+
+	importObj := wasmer.NewImportObject()
+	importObj.Register(
+		"env",
+		map[string]wasmer.IntoExtern{
+			"GetMeasurementInt": wasmer.NewFunctionWithEnvironment(
+				store,
+				wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32),
+					wasmer.NewValueTypes(wasmer.I32)),
+				env,
+				GetMeasurementInt,
+			),
+			"GetMeasurementBytes": wasmer.NewFunctionWithEnvironment(
+				store,
+				wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32),
+					wasmer.NewValueTypes(wasmer.I32)),
+				env,
+				GetMeasurementBytes,
+			),
+			"ForwardInterest": wasmer.NewFunctionWithEnvironment(
+				store,
+				wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32),
+					wasmer.NewValueTypes()),
+				env,
+				ForwardInterest,
+			),
+			"SetMeasurementInt": wasmer.NewFunctionWithEnvironment(
+				store,
+				wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32),
+					wasmer.NewValueTypes()),
+				env,
+				SetMeasurementInt,
+			),
+			"AddToMeasurementInt": wasmer.NewFunctionWithEnvironment(
+				store,
+				wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32),
+					wasmer.NewValueTypes()),
+				env,
+				AddToMeasurementInt,
+			),
+			"SetMeasurementBytes": wasmer.NewFunctionWithEnvironment(
+				store,
+				wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32),
+					wasmer.NewValueTypes()),
+				env,
+				SetMeasurementBytes,
+			),
+		},
+	)
+	// Used by WASI
+	importObj.Register("wasi_snapshot_preview1",
+		map[string]wasmer.IntoExtern{
+			"proc_exit": wasmer.NewFunction(
+				store,
+				wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32), wasmer.NewValueTypes()),
+				func(args []wasmer.Value) ([]wasmer.Value, error) {
+					fmt.Printf("WASM quited with %d\n", args[0].I32())
+					return []wasmer.Value{}, nil
+				},
+			),
+		})
 
 	// Instantiates the WebAssembly module.
-	instance, err = wasmer.NewInstanceWithImports(bytes, imports)
+	instance, err := wasmer.NewInstance(module, importObj)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer instance.Close()
+	env.inst = instance
 
-	measure = make(map[string]interface{})
-	measure["counter"] = 0
-	measure["hash"] = []byte{0, 0, 0, 0}
+	env.memory, err = instance.Exports.GetMemory("memory")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	env.allocate, err = instance.Exports.GetFunction("Allocate")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	env.deallocate, err = instance.Exports.GetFunction("Deallocate")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	env.afterReceiveInterest, err = instance.Exports.GetFunction("AfterReceiveInterest")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	callAfterReceiveInterest("hello")
-	callAfterReceiveInterest("world")
+	callAfterReceiveInterest(env, "hello")
+	callAfterReceiveInterest(env, "world")
 
 	fmt.Printf("counter = %v\n", measure["counter"])
 	fmt.Printf("hash = %x\n", measure["hash"])
